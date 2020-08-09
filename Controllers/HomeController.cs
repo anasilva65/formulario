829863660c85efab3328894652ee51f3f587b84d
Anna Silva <annadasilva89@hotmail.com>
2020-08-09 12:17:57 -0300

    FormularioDeValidacao

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using formulario.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace formulario.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Cadastro(CadastroModel resposta)
+        {
+            try
+            {
+                return RedirectToAction("Obrigado");
+            }
+            catch (Exception)
+            {
+                return View();
+            }
+        }
+
+        public IActionResult Obrigado()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/CadastroModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace formulario.Models
+{
+    public class CadastroModel
+    {
+        public string Nome { get; set; }
+
+        public string Idade { get; set; }
+
+        public string Telefone { get; set; }
+    }
+}
